71b4a8883c8f2e36a917ce6fc2edc980917429a2
zhh <617146873@qq.com>
2017-02-08 22:19:43 +0800

    admin

--- README.md
+++ README.md
@@ -1,1 +1,31 @@
-"# tinyFTP" 
+# tinyFTP
+
+一个小型的FTP服务器
+
+## 已有功能
+
+1. 浏览目录，下载文件
+
+## 待添加功能
+
+1. 上传文件
+2. 显示更多的文件信息，如文件大小，创建时间等。
+3. 帐号登陆以及帐号管理
+
+##  项目搭建
+
+```sh
+# 下载源代码
+git clone https://github.com/zhangh42/tinyFTP.git
+# 安装依赖
+cd tinyPTF & npm install
+# 运行
+npm start
+```
+
+
+
+
+
+
+
--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.redirect('/users/admin');
 });
+
+router.get('/admin', function(req, res, next){
+  res.render('admin');
+})
 
 module.exports = router;
